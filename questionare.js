9d29be2d9f4cb5eb9a7ba47047e680e1fe25e4b6
Alex Chheng <Alex.Chheng@student.csulb.edu>
2022-04-30 15:56:34 -0700

    Spiritual Questionare

--- questionare.js
+++ questionare.js
@@ -39,7 +39,7 @@ function getPsychological(){
     var amount = 0;
 
     // LOOP FOR GOING THROUGH ALL QUESTIONS
-    for(var i = 1; i <= 2; i++) {
+    for(var i = 1; i <= 11; i++) {
         var radiosName = document.getElementsByName('psychological'+i);
   
         //LOOP FOR CHECKING ANSWERS INSIDE EACH RADIO
@@ -104,4 +104,40 @@ function getEmotional(){
         }
     }
     outputEmotional.innerText = amount ? `Your total is ${amount}` : `You have not select an answers`;
+}
+
+function getSpiritual(){
+    //CORRECT ANSWERS
+    var amount = 0;
+
+    // LOOP FOR GOING THROUGH ALL QUESTIONS
+    for(var i = 1; i <= 12; i++) {
+        var radiosName = document.getElementsByName('spiritual'+i);
+  
+        //LOOP FOR CHECKING ANSWERS INSIDE EACH RADIO
+        for(var j = 0; j < radiosName.length; j++) {
+            var radiosValue = radiosName[j];
+            if(radiosValue.checked){ // An explanation for this line is nice, I don't know why it work without this 
+                //(https://teamtreehouse.com/community/javascript-project-simple-quiz-with-radio-buttons)
+                switch(radiosValue.value) {
+                    case "option5":
+                        amount+=5;
+                        break;
+                    case "option4":
+                        amount+=4;
+                        break;
+                    case "option3":
+                        amount+=3;
+                        break;
+                    case "option2":
+                        amount+=2;
+                        break;
+                    case "option1":
+                        amount+=1;
+                        break;
+                }
+            }
+        }
+    }
+    outputSpiritual.innerText = amount ? `Your total is ${amount}` : `You have not select an answers`;
 }
